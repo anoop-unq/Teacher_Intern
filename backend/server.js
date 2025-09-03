6c28406289bff14f7aef4858311d06f71ca0ad97
anoop-unq <padakantianoop7@gmail.com>
2025-09-03 14:19:19 +0530

    ip

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,9 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  // origin: 'http://localhost:5173',
+  origin: 'https://teacher-intern.vercel.app',
+
 
   credentials: true,
    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
